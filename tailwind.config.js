f3922e2739310fa3e0c5a66302ed6b0b3b3b3414
uShire <afgregana@up.edu.ph>
2023-11-16 15:10:50 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dilaw: "#f9c73d",
+        asul: "#005e7f",
+        light: "#ffffff",
+        dark: "#2b2b2b",
+      },
+    },
+  },
+  plugins: [],
+};
